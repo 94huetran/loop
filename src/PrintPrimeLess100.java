5946a4b7eca7218155dd431d4e2551b39656c292
Tran Thu Hue <94huetran@gmail.com>
2018-06-15 15:15:51 +0700

    add PrintPrimeLess100

---
+++ src/PrintPrimeLess100.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintPrimeLess100 {
+    public static void main(String[] args) {
+        for (int a = 2 ; a < 100; a++) {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(a)) {
+                if (a % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            } if (check)
+                System.out.println("The Prime number:" + a);
+   }
+    }
+}
