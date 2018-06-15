1785663ed3bb895ac625f1b8dd7f817901082a2a
Tran Thu Hue <94huetran@gmail.com>
2018-06-15 17:14:56 +0700

    print first 20 prime numbers (skeleton)

---
+++ src/Print20PrimeNumber.java
@@ -0,0 +1,16 @@
+public class Print20PrimeNumber {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 1;
+        while (count < 20) {
+            boolean isPrime = true;
+            //TODO: determine n is prime number or not
+            if (isPrime) {
+                System.out.println(n);
+                count++;
+            }
+            n++;
+        }
+    }
+}
+
